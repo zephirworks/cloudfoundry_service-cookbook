253e207c79b4d5c2b9187010f2577418c30d6187
Andrea Campi <andrea.campi@zephirworks.com>
2012-12-28 00:26:32 +0100

    Release 1.1.0: add new facilities to DRY up services.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "andrea.campi@zephirworks.com"
 license          "Apache 2.0"
 description      "Base cookbook for cloudfoundry service cookbooks"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.1"
+version          "1.1.0"
 
 %w{ ubuntu }.each do |os|
   supports os
